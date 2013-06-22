a7f43b13932993948ff7b0f7b77d783da276e43f
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-06-22 11:32:53 +0200

    Added working name input.

--- src/screen/ScoreScreen.java
+++ src/screen/ScoreScreen.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
+import engine.Cooldown;
 import engine.Core;
 import engine.Score;
 
@@ -21,8 +22,11 @@ public class ScoreScreen extends Screen {
 	private int livesRemaining;
 	private int bulletsShot;
 	private int shipsDestroyed;
+	private List<Score> highScores;
 	private boolean isNewRecord;
 	private char[] name;
+	private int nameCharSelected;
+	private Cooldown selectionCooldown;
 
 	/**
 	 * Constructor, establishes the properties of the screen.
@@ -46,26 +50,17 @@ public class ScoreScreen extends Screen {
 		this.shipsDestroyed = shipsDestroyed;
 		this.isNewRecord = false;
 		this.name = "AAA".toCharArray();
+		this.nameCharSelected = 0;
+		this.selectionCooldown = Core.getCooldown(200);
+		this.selectionCooldown.reset();
 
 		try {
-			List<Score> highScores = Core.getFileManager().loadHighScores();
-			if (highScores.size() < 7) {
+			this.highScores = Core.getFileManager().loadHighScores();
+			if (highScores.size() < 7
+					|| highScores.get(highScores.size() - 1).getScore() < this.score)
 				this.isNewRecord = true;
-				// TODO placeholder player name.
-				highScores.add(new Score("AAA", score));
-				Collections.sort(highScores);
 
-				Core.getFileManager().saveHighScores(highScores);
-			} else if (highScores.get(highScores.size() - 1).getScore() < this.score) {
-				this.isNewRecord = true;
-				// TODO placeholder player name.
-				highScores.add(new Score("AAA", score));
-				Collections.sort(highScores);
-				highScores.remove(highScores.size() - 1);
-
-				Core.getFileManager().saveHighScores(highScores);
-			}
-		} catch (NumberFormatException | IOException e) {
+		} catch (IOException e) {
 			logger.warning("Couldn't load high scores!");
 		}
 	}
@@ -91,14 +86,57 @@ public class ScoreScreen extends Screen {
 			if (inputManager.isKeyDown(KeyEvent.VK_ESCAPE)) {
 				this.returnCode = 1;
 				this.isRunning = false;
+				if (this.isNewRecord)
+					saveScore();
 			}
 			// Play again.
 			else if (inputManager.isKeyDown(KeyEvent.VK_SPACE)) {
 				this.returnCode = 2;
 				this.isRunning = false;
+				if (this.isNewRecord)
+					saveScore();
+			}
+
+			if (this.isNewRecord && this.selectionCooldown.checkFinished()) {
+				if (inputManager.isKeyDown(KeyEvent.VK_RIGHT)) {
+					this.nameCharSelected = this.nameCharSelected == 2 ? 0
+							: this.nameCharSelected + 1;
+					this.selectionCooldown.reset();
+				}
+				if (inputManager.isKeyDown(KeyEvent.VK_LEFT)) {
+					this.nameCharSelected = this.nameCharSelected == 0 ? 2
+							: this.nameCharSelected - 1;
+					this.selectionCooldown.reset();
+				}
+				if (inputManager.isKeyDown(KeyEvent.VK_UP)) {
+					this.name[this.nameCharSelected] = (char) (this.name[this.nameCharSelected] == 90 ? 65
+							: this.name[this.nameCharSelected] + 1);
+					this.selectionCooldown.reset();
+				}
+				if (inputManager.isKeyDown(KeyEvent.VK_DOWN)) {
+					this.name[this.nameCharSelected] = (char) (this.name[this.nameCharSelected] == 65 ? 90
+							: this.name[this.nameCharSelected] - 1);
+					this.selectionCooldown.reset();
+				}
 			}
 		}
 
+	}
+
+	/**
+	 * Saves the score as a high score.
+	 */
+	private void saveScore() {
+		highScores.add(new Score(new String(this.name), score));
+		Collections.sort(highScores);
+		if (highScores.size() > 7)
+			highScores.remove(highScores.size() - 1);
+
+		try {
+			Core.getFileManager().saveHighScores(highScores);
+		} catch (IOException e) {
+			logger.warning("Couldn't load high scores!");
+		}
 	}
 
 	/**
@@ -111,7 +149,8 @@ public class ScoreScreen extends Screen {
 			drawManager.drawScoreScreen(this, this.score, this.livesRemaining,
 					this.shipsDestroyed, (float) this.shipsDestroyed
 							/ this.bulletsShot,
-					this.inputDelay.checkFinished(), this.name, 0);
+					this.inputDelay.checkFinished(), this.name,
+					this.nameCharSelected);
 		else
 			drawManager
 					.drawScoreScreen(this, this.score, this.livesRemaining,
