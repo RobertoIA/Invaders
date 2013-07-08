dfcaf0b5e2efa6d1f2a4a3c937805ccef890aab5
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-07-08 13:11:05 +0200

    Added delay time between screen changes.

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -31,8 +31,10 @@ public class GameScreen extends Screen {
 	private static final int BONUS_SHIP_INTERVAL = 20000;
 	/** Maximum variance in the time between bonus ship's appearances. */
 	private static final int BONUS_SHIP_VARIANCE = 10000;
-	/** Time until bonus ship explosion dissapears. */
+	/** Time until bonus ship explosion disappears. */
 	private static final int BONUS_SHIP_EXPLOSION = 500;
+	/** Time from finishing the level to screen change. */
+	private static final int SCREEN_CHANGE_INTERVAL = 1500;
 	/** Height of the interface separation line. */
 	private static final int SEPARATION_LINE_HEIGHT = 40;
 
@@ -48,8 +50,10 @@ public class GameScreen extends Screen {
 	private EnemyShip enemyShipSpecial;
 	/** Minimum time between bonus ship appearances. */
 	private Cooldown enemyShipSpecialCooldown;
-	/** Time until bonus ship explosion dissapears. */
+	/** Time until bonus ship explosion disappears. */
 	private Cooldown enemyShipSpecialExplosionCooldown;
+	/** Time from finishing the level to screen change. */
+	private Cooldown screenFinishedCooldown;
 	/** Set of all bullets fired by on screen ships. */
 	private Set<Bullet> bullets;
 	/** Current score. */
@@ -62,6 +66,8 @@ public class GameScreen extends Screen {
 	private int shipsDestroyed;
 	/** Moment the game starts. */
 	private long gameStartTime;
+	/** Checks if the level is finished. */
+	private boolean levelFinished;
 
 	/**
 	 * Constructor, establishes the properties of the screen.
@@ -105,6 +111,7 @@ public class GameScreen extends Screen {
 		this.enemyShipSpecialCooldown.reset();
 		this.enemyShipSpecialExplosionCooldown = Core
 				.getCooldown(BONUS_SHIP_EXPLOSION);
+		this.screenFinishedCooldown = Core.getCooldown(SCREEN_CHANGE_INTERVAL);
 		this.bullets = new HashSet<Bullet>();
 
 		// Special input delay / countdown.
@@ -133,7 +140,7 @@ public class GameScreen extends Screen {
 	protected final void update() {
 		super.update();
 
-		if (this.inputDelay.checkFinished()) {
+		if (this.inputDelay.checkFinished() && !this.levelFinished) {
 
 			if (!this.ship.isDestroyed()) {
 				boolean moveRight = inputManager.isKeyDown(KeyEvent.VK_RIGHT)
@@ -187,8 +194,15 @@ public class GameScreen extends Screen {
 		cleanBullets();
 		draw();
 
-		if (this.enemyShipFormation.isEmpty() || this.lives == 0)
+		if ((this.enemyShipFormation.isEmpty() || this.lives == 0)
+				&& !this.levelFinished) {
+			this.levelFinished = true;
+			this.screenFinishedCooldown.reset();
+		}
+
+		if (this.levelFinished && this.screenFinishedCooldown.checkFinished())
 			this.isRunning = false;
+
 	}
 
 	/**
