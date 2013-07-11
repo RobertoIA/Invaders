47087d29c0a46ccc143041da8455050824d33378
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-07-11 20:15:14 +0200

    Fixed margins.

--- src/engine/Frame.java
+++ src/engine/Frame.java
@@ -39,7 +39,7 @@ public class Frame extends JFrame {
 		setVisible(true);
 
 		Insets insets = getInsets();
-		this.width = width - insets.left + insets.right;
+		this.width = width - insets.left - insets.right;
 		this.height = height - insets.top + insets.bottom;
 		setTitle("Invaders");
 
